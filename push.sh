ee343e2e92b2d63639eda5465fb2f2c4d008f49a
Aravind Viswanathan <aravind.viswanathan@fourkites.com>
2024-09-20 16:20:07 +0530

    Push scripts

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the current branch name
+current_branch=$(git branch --show-current)
+
+# Check if the branch name is "develop" or "main" or "master"  (case-sensitive)
+if [[ "$current_branch" == "develop" || "$current_branch" == "main" || "$current_branch" == "master" || "$current_branch" == "qat-release" ]]; then
+  echo "IDIOT!! IDIOT!! IDIOT!! This is a protected branch.. Do not push here"
+  say "Protected branch.. PUSH FAILED"
+else
+  git push
+fi
+
