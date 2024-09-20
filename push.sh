8db8d4de157c81d0f12ead4796b9a54cda5fc395
Aravind Viswanathan <aravind.viswanathan@fourkites.com>
2024-09-20 16:23:29 +0530

    Updated additonal branches as protected

--- push.sh
+++ push.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Avoid pushing to specified branches by mistake.
+
 # Get the current branch name
 current_branch=$(git branch --show-current)
 
 # Check if the branch name is "develop" or "main" or "master"  (case-sensitive)
-if [[ "$current_branch" == "develop" || "$current_branch" == "main" || "$current_branch" == "master" || "$current_branch" == "qat-release" ]]; then
+if [[ "$current_branch" == "develop" || "$current_branch" == "main" || "$current_branch" == "master" || "$current_branch" == "qat-release"i || "$current_branch" == "staging" || "$current_branch" == "production" ]]; then
   echo "IDIOT!! IDIOT!! IDIOT!! This is a protected branch.. Do not push here"
   say "Protected branch.. PUSH FAILED"
 else
--- push_upstream.sh
+++ push_upstream.sh
@@ -4,10 +4,10 @@
 current_branch=$(git branch --show-current)
 
 # Check if the branch name is "develop" or "main" or "master"  (case-sensitive)
-if [[ "$current_branch" == "develop" || "$current_branch" == "main" || "$current_branch" == "master" || "$current_branch" == "qat-release" ]]; then
+if [[ "$current_branch" == "develop" || "$current_branch" == "main" || "$current_branch" == "master" || "$current_branch" == "qat-release" || "$current_branch" == "staging" || "$current_branch" == "production" ]]; then
   echo "IDIOT!! IDIOT!! IDIOT!! This is a protected branch.. Do not push here"
   say "Protected branch.. PUSH FAILED"
 else
-  git push --set-upstream origin $(git_current_branch)
+  git push --set-upstream origin $current_branch
 fi
 
