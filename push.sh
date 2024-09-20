575f20525551c73dd4bea259990c94b5669428cd
Aravind Viswanathan <aravind.viswanathan@fourkites.com>
2024-09-20 16:28:33 +0530

    Optimised the script

--- push.sh
+++ push.sh
@@ -5,11 +5,17 @@
 # Get the current branch name
 current_branch=$(git branch --show-current)
 
-# Check if the branch name is "develop" or "main" or "master"  (case-sensitive)
-if [[ "$current_branch" == "develop" || "$current_branch" == "main" || "$current_branch" == "master" || "$current_branch" == "qat-release"i || "$current_branch" == "staging" || "$current_branch" == "production" ]]; then
+# Define an array of protected branch names
+protected_branches=("develop" "main" "master" "qat-release" "staging" "production")
+
+# Check if the current branch is in the protected branches array
+if [[ " ${protected_branches[@]} " =~ " ${current_branch} " ]]; then
   echo "IDIOT!! IDIOT!! IDIOT!! This is a protected branch.. Do not push here"
   say "Protected branch.. PUSH FAILED"
 else
-  git push
+  if [[ "$0" == *"gpsup"* ]]; then
+    git push --set-upstream origin "$current_branch"
+  else
+    git push
+  fi
 fi
-
